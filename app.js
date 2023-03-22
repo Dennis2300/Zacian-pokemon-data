f3054c6f28233339a0c7320bf72ee97b8433cc18
Dennis2300 <124138161+Dennis2300@users.noreply.github.com>
2023-03-22 08:37:54 +0100

    update 2

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ const zacian = {
     weight: "110.0",
     gender: "Unknown",
     generation: "Generation VIII",
-    height: "2.8"
+    height: "2.8",
+    health: "6",
+    attack: "8",
+    defense: "7",
+    specialAttack: "7",
+    specialDefense:"9",
+    speed:"5",
 }
 
 function initApp() {
@@ -24,11 +30,12 @@ function initApp() {
 
 function showPokemon(pokemon) {
 console.log("Pokemon: Zacian");
-const pokemonZacian = /*html*/ `
+const pokemonZacian = /*HTML*/ `
+<article1>
 <image src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/888.png"></image><br>
 <p>${pokemon.description}</p>
 <li>Pokemon Name: ${pokemon.name}</li>
-<li>Pokedex Nr: #‡${pokemon.dexIndex}</li>
+<li>Pokedex Nr: #${pokemon.dexIndex}</li>
 <li>Category: ${pokemon.category}</li>
 <li>Ability: ${pokemon.ability}</li>
 <li>Type: ${pokemon.type}</li>
@@ -37,6 +44,19 @@ const pokemonZacian = /*html*/ `
 <li>Gender: ${pokemon.gender}</li>
 <li>Generation: ${pokemon.generation}</li>
 <li>Height: ${pokemon.height} meter</li>
+</article1>
+`
+const pokemonZacianStats = /*HTML*/`
+<article2>
+<h2>Pokemon Stats</h2>
+<li>Health: ${pokemon.health}</li>
+<li>Speed: ${pokemon.speed}</li>
+<li>Attack: ${pokemon.attack}</li>
+<li>Defense: ${pokemon.defense}</li>
+<li>Special Attack: ${pokemon.specialAttack}</li>
+<li>Special Defense: ${pokemon.specialDefense}</li>
+</article2>
 `
 document.querySelector("#pokemons").insertAdjacentHTML("beforeend", pokemonZacian);
+document.querySelector("#pokemons").insertAdjacentHTML("beforeend", pokemonZacianStats);
 }
