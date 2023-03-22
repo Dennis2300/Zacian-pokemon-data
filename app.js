59a384b5c799630cdd6952e3719783bad8a8827b
Dennis2300 <124138161+Dennis2300@users.noreply.github.com>
2023-03-22 09:37:52 +0100

    update 4

--- app.js
+++ app.js
@@ -2,20 +2,22 @@
 window.addEventListener("load", initApp)
 
 const zacian = {
-    image:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/888.png",
     description: 
-    "Known as a legendary hero, this Pokémon absorbs metal particles, transforming them into a weapon it uses to battle.",
+    "Known as a legendary hero, this Pokémon absorbs metal particles, transforming them into a weapon it uses to battle. Now armed with a weapon it used in ancient times, this Pokémon needs only a single strike to fell even Gigantamax Pokémon",
     name: "Zacian",
-    category:"Warrior",
     ability: "Intrepid Sword",
+    footprint: "Unknown",
     dexIndex: "0888",
     type: "Fairy",
-    weakness: "Steel",
-    weakness2:"Poison",
-    weight: "110.0",
+    subtype:"Steel",
+    weakness: "Fire",
+    weakness2:"Ground",
     gender: "Unknown",
-    generation: "Generation VIII",
+    weight: "110.0",
     height: "2.8",
+    generation: "Generation VIII",
+    spilVersion:"Pokemon Sword",
+    canEvolve:"false",
     health: "6",
     attack: "8",
     defense: "7",
@@ -33,20 +35,24 @@ console.log("Pokemon: Zacian");
 const pokemonZacian = /*HTML*/ `
 <article>
 <h1>Zacian</h1>
-<image src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/888.png"></image><br>
+<image src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/888_f2.png"></image><br>
 <div id="parent">
     <p>${pokemon.description}</p>
     <li>Pokemon Name: ${pokemon.name}</li>
     <li>Pokedex Nr: #${pokemon.dexIndex}</li>
-    <li>Category: ${pokemon.category}</li>
+    <li>Footprint: ${pokemon.footprint}</li>
     <li>Ability: ${pokemon.ability}</li>
     <li>Type: ${pokemon.type}</li>
+    <li>Subtype ${pokemon.subtype}</li>
     <li>Weakness: ${pokemon.weakness} & ${pokemon.weakness2}</li>
+    <li>Gender: ${pokemon.gender}</li>
     <li>Weight: ${pokemon.weight} kg</li>
+    <li>Height: ${pokemon.height} meter</li>
     <li>Gender: ${pokemon.gender}</li>
     <li>Generation: ${pokemon.generation}</li>
-    <li>Height: ${pokemon.height} meter</li>
-</div>
+    <li>Game Version: ${pokemon.spilVersion}</li>
+    <li>Evolve: ${pokemon.canEvolve}</li>
+    </div>
 </article>
 `
 const pokemonZacianStats = /*HTML*/`
